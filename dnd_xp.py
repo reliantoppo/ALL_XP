8aade6b2e7e90d9ed9c6faa6fae1163b14e71246
neil aaron madaczky <nmadaczk@neils-MacBook-Pro.local>
2020-04-18 20:03:27 -0400

    Reformatted the final print statement with formatting

--- dnd_xp.py
+++ dnd_xp.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 current_xp = input("Current experience: ")
+current_xp = int(current_xp)
 unmod_xp = input("Enter unmodified experience: ")
-mod_xp = input("Enter modifiable (10%) experience: ")*1.1
+unmod_xp = int(unmod_xp)
+mod_xp = input("Enter modifiable (10%) experience: ")
+mod_xp = int(mod_xp) * (1.1)
+sum = (unmod_xp) + (mod_xp) + (current_xp)
 
-print("Unmodified experience: " + str(unmod_xp))
-print("Modified experience: " + str(mod_xp))
-print("Those plus your previous experience brings your new total experince to...:  "+ int(unmod_xp) + int(mod_xp) + int(current_xp))
+print("Your new total experience is: {}".format(sum))
