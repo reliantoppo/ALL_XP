27984051219ed6704a236ec3a6768dd0b852b927
Neil Madaczky <workwithnmadaczky@gmail.com>
2021-01-31 23:51:38 -0500

    Update dnd_xp.py
    Updated to take additional percentages, instead of just 10%. Still need to get the math portion corrected, so it will actually handle percentages better.

--- dnd_xp.py
+++ dnd_xp.py
@@ -1,11 +1,12 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
 current_xp = input("Current experience: ")
 current_xp = int(current_xp)
 unmod_xp = input("Enter unmodified experience: ")
 unmod_xp = int(unmod_xp)
-mod_xp = input("Enter modifiable (10%) experience: ")
-mod_xp = int(mod_xp) * (1.1)
+modifier = input("What if any, is your modifier percentage? (0, 5, 10, 15, 20): ")
+mod_xp = input("Enter modifiable experience: ")
+mod_xp = int(mod_xp) * int(modifier)
 sum = (unmod_xp) + (mod_xp) + (current_xp)
 
 print("Your new total experience is: {}".format(sum))
